3a3bccc2bd35f5cb1828f3119bc54c0eed59f421
Usman Tahir <Usman.tahir@synergy-it.com>
2021-12-23 15:04:49 +0500

    code changes

--- src/Services/Catalog/Catalog.API/Startup.cs
+++ src/Services/Catalog/Catalog.API/Startup.cs
@@ -36,7 +36,6 @@ namespace Catalog.API
             });
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
