cec37fc9cdd3540e209ecd23b963a87ff485eea1
Usman Tahir <Usman.tahir@synergy-it.com>
2021-12-26 18:21:04 +0500

    Code in Infrastructure layer

---
+++ src/Services/Ordering/Ordering.Infrastructure/Persistance/OrderContextSeed.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Logging;
+using Ordering.Domain.Entities;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ordering.Infrastructure.Persistance
+{
+    class OrderContextSeed
+    {
+        public static async Task SeedAsync(OrderContext orderContext, ILogger<OrderContextSeed> logger)
+        {
+            if (!orderContext.Orders.Any())
+            {
+                orderContext.Orders.AddRange(GetPreconfiguredOrders());
+                await orderContext.SaveChangesAsync();
+                logger.LogInformation("Seed database associated with context {DbContextName}", typeof(OrderContext).Name);
+            }
+        }
+
+        private static IEnumerable<Order> GetPreconfiguredOrders()
+        {
+            return new List<Order>
+            {
+                new Order() {UserName = "swn", FirstName = "Usman", LastName = "Tahir", EmailAddress = "utahir604@gmail.com", AddressLine = "Rawalpindi", Country = "Pakistan", TotalPrice = 350 }
+            };
+        }
+    }
+}
