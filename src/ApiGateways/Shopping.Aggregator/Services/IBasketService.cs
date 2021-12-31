698ac5d11416deb53f89fd36dd964c9750c031b6
Usman Tahir <Usman.tahir@synergy-it.com>
2021-12-31 16:44:08 +0500

    add shopping.aggregator in docker compose

---
+++ src/ApiGateways/Shopping.Aggregator/Services/IBasketService.cs
@@ -0,0 +1,11 @@
+﻿
+using Shopping.Aggregator.Models;
+using System.Threading.Tasks;
+
+namespace Shopping.Aggregator.Services
+{
+    public interface IBasketService
+    {
+        Task<BasketModel> GetBasket(string userName);
+    }
+}
